647c2734ac5193fc04a687d4b5c13354af9740bf
albert-lee-2021 <albert.lee@alumni.stevens.edu>
2021-12-29 10:23:24 -0500

    backend cleanup

--- src/main/java/com/revature/DAO/PostDao.java
+++ src/main/java/com/revature/DAO/PostDao.java
@@ -1,16 +1,16 @@
 package com.revature.DAO;
 
-import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import com.revature.models.Post;
-import com.revature.models.User;
 
 @Repository
 public interface PostDao extends JpaRepository<Post, Integer> {
-	Post save(Post post);
-	List<Post> findAll();
+	// Post save(Post post); not needed as crud repository already has it, avoids error message
+	// List<Post> findByParentId(int id);
+	List<Post> findByParentIdIsNull();
+	
 }
