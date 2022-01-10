c57d1507939c54aa38bb8e7ac429ee57365665f0
Falcon-JasonM <87274520+Falcon-JasonM@users.noreply.github.com>
2022-01-10 10:58:09 -0700

    1/10 AM tests

---
+++ src/test/java/com/revature/vibez/UserControllerTests.java
@@ -0,0 +1,58 @@
+package com.revature.vibez;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+import java.util.List;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import com.revature.controllers.UserController;
+import com.revature.models.User;
+import com.revature.services.UserService;
+
+@SpringBootTest
+public class UserControllerTests {
+	
+	@Autowired
+	public UserService us;
+	
+	@Autowired
+	public UserController uc;
+	
+	@Test
+	public void gets_user_if_id_given(){
+		int id = 1;
+		User u = uc.getUserById(id);
+		assertNotNull(u);
+	}
+	
+	@Test
+	public void gets_all_users_if_username_null() {
+		String username = null;
+		String email = null;
+		List<User> users = uc.getAllUsers(username, email);	
+		int size = users.size();
+		assertEquals(size, 14);	
+	}
+	
+	@Test
+	public void creates_user_if_user_given() {
+		User u = new User();
+
+		u.setFirstName("Joe");
+		u.setLastName("Yooser");
+		u.setEmail("testmail@testing.com");
+		u.setPassword("joepass");
+		u.setUsername("joeusername");
+		
+		ResponseEntity<String> response = uc.createUser(u);
+		assertEquals(response, "<201 CREATED Created,[]>");
+	}
+}
